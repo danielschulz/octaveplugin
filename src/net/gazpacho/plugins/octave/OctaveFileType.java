005046fbc9e69cff1c87337754dfab9653cff663
Tom Conder <tomc@gazpacho.net>
2012-09-12 16:46:41 -0500

    renamed package to net.gazpacho.plugins.octave

---
+++ src/net/gazpacho/plugins/octave/OctaveFileType.java
@@ -0,0 +1,45 @@
+package net.gazpacho.plugins.octave;
+
+import com.intellij.lang.Language;
+import com.intellij.openapi.fileTypes.LanguageFileType;
+import icons.OctaveIcons;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+
+public class OctaveFileType extends LanguageFileType {
+    public static final LanguageFileType OCTAVE_FILE_TYPE = new OctaveFileType();
+    public static final Language OCTAVE_LANGUAGE = OCTAVE_FILE_TYPE.getLanguage();
+    @NonNls public static final String DEFAULT_EXTENSION = "m";
+
+    private OctaveFileType() {
+        super(OctaveLanguage.getInstance());
+    }
+
+    @NotNull
+    @NonNls
+    @Override
+    public String getName() {
+        return "Octave";
+    }
+
+    @NotNull
+    @NonNls
+    @Override
+    public String getDescription() {
+        return OctaveBundle.message("octave.files.file.type.description");
+    }
+
+    @NotNull
+    @NonNls
+    @Override
+    public String getDefaultExtension() {
+        return DEFAULT_EXTENSION;
+    }
+
+    @Override
+    public Icon getIcon() {
+        return OctaveIcons.iconOctave;
+    }
+}
---
+++ src/net/gazpacho/plugins/octave/OctaveLanguage.java
@@ -0,0 +1,20 @@
+package net.gazpacho.plugins.octave;
+
+import com.intellij.lang.Language;
+
+public class OctaveLanguage extends Language {
+    private static final OctaveLanguage INSTANCE = new OctaveLanguage();
+
+    private OctaveLanguage() {
+        super("Octave");
+    }
+
+    public static OctaveLanguage getInstance() {
+        return INSTANCE;
+    }
+
+    @Override
+    public boolean isCaseSensitive() {
+        return true;
+    }
+}
