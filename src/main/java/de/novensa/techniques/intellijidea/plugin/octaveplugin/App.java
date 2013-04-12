125880e8bb7d0d34254ace8a2602608e0f1d9785
Daniel Schulz <danielschulz2005@hotmail.com>
2013-04-12 19:08:03 +0200

    init

---
+++ src/main/java/de/novensa/techniques/intellijidea/plugin/octaveplugin/App.java
@@ -0,0 +1,13 @@
+package de.novensa.techniques.intellijidea.plugin.octaveplugin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
